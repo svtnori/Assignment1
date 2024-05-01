80762300e721dc78adaf37d5905babe54ad1c7d4
svtnori <noriellumawod@gmail.com>
2024-05-01 19:51:32 +0800

    ACTIVITY 9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAvImG9OKQhv_GA9-t8-3Jlc2CWB01uxKg",
-    authDomain: "activity-8-pwa-firebase.firebaseapp.com",
-    projectId: "activity-8-pwa-firebase",
-    storageBucket: "activity-8-pwa-firebase.appspot.com",
-    messagingSenderId: "329322552620",
-    appId: "1:329322552620:web:3e18ad8fc8309fb461e089"
+    apiKey: "AIzaSyBIrbFn5X1_ibMtZZJHWGj5VIxb-zjWfgg",
+  authDomain: "activity-09-4826b.firebaseapp.com",
+  projectId: "activity-09-4826b",
+  storageBucket: "activity-09-4826b.appspot.com",
+  messagingSenderId: "126186796873",
+  appId: "1:126186796873:web:bf407610bd0e4f770e4c98"
   }
 };
